07fa0f757be1eca4a125a22b4754af08b93783e4
Aske Gudmand-Høyer <aske@kultunaut.dk>
2025-11-09 13:23:59 +0100

    Add authentication features with email login and OTP verification
    - Implement email login with OTP verification in auth.remote.ts
    - Add JWT handling for email verification
    - Update package.json to include new dependencies for authentication
    - Create login and opt pages for user interaction
    - Configure prettier and add relevant plugins
    - Add .env.example for environment variable setup
    - Update .gitignore to exclude sensitive files

---
+++ src/lib/auth/auth.remote.ts
@@ -0,0 +1,25 @@
+import { form, getRequestEvent } from "$app/server";
+import * as v from "valibot";
+import { getVerificationJWT, sendOTPCode, verifyOTP } from "./auth";
+import { redirect } from "@sveltejs/kit";
+import { resolve } from "$app/paths";
+import { createJWTSignatureMessage, encodeJWT, joseAlgorithmHS256 } from "@oslojs/jwt";
+
+
+export const loginWithEmail = form(v.object({email: v.pipe(v.string(), v.email())}), async ({email}) => {
+    sendOTPCode(email);
+    const { cookies } = getRequestEvent();
+
+    cookies.set("verification", await getVerificationJWT(email), {
+        path: "/",
+        expires: new Date(Date.now()+60*60),
+    })
+    redirect(302, resolve("/opt"))
+})
+export const verifyOTPForm = form(v.object({otp: v.number()}), ({otp})=> {
+    if(verifyOTP(otp)){
+        const {cookies} = getRequestEvent()
+        const verificationJWT = cookies.get("verification")
+        cookies.set()
+    }
+})
---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { loginWithEmail } from "$lib/auth/auth.remote";
+  import type { PageProps } from "./$types";
+
+  let { data }: PageProps = $props();
+</script>
+
+<main class="flex min-h-screen justify-center items-center">
+  <form {...loginWithEmail}>
+    <formfield class="flex flex-col gap-4">
+      <div>
+        <label class="block text-sm" for="email">Email</label>
+        <input
+          class="border p-1 text-sm"
+          placeholder="test@example.com"
+          {...loginWithEmail.fields.email.as("email")}
+        />
+      </div>
+      <button type="submit" class="bg-primary">Submit</button>
+    </formfield>
+  </form>
+</main>
