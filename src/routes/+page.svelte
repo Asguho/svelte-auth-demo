1222060340624df0ad618d858ada9d7caa04390a
Aske Gudmand-Høyer <aske@kultunaut.dk>
2025-11-09 15:40:01 +0100

    Refactor user greeting and improve formatting in main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,13 @@
 	import { getUser, signOut } from '$lib/auth/auth.remote';
 
 	let user = $derived(await getUser());
-	$inspect(user);
 </script>
 
-<main class="flex flex-col items-center gap-2">
+<main class="mt-20 flex flex-col items-center gap-2">
 	<h1 class="text-3xl">Welcome to SvelteKit</h1>
-	<p>
-		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-	</p>
 
 	{#if user}
-		<p>your email is {user.email} and your id is {user.id}</p>
+		<p>Your email is {user.email} and your id is {user.id}</p>
 		<form {...signOut}>
 			<button class="text-primary hover:underline" type="submit">Sign out</button>
 		</form>
