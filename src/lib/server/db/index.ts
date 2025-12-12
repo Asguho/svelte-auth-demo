612c23accc410c3b182d9f39f445172adb033fdf
Aske Gudmand-Høyer <aske@kultunaut.dk>
2025-12-12 13:22:01 +0100

    chore: update dependencies and refactor database connection to use drizzle-orm with bun-sql

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,8 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/bun-sql';
+import { SQL } from 'bun';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const client = new SQL(env.DATABASE_URL);
 
-const client = postgres(env.DATABASE_URL);
-
-export const db = drizzle(client, { schema });
+export const db = drizzle({ client, schema });
