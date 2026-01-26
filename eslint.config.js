edbe0e54b5ac6f5cd64f941d1a3a869000614e50
Aske Gudmand-Høyer <aske@kultunaut.dk>
2026-01-26 18:35:47 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import prettier from 'eslint-config-prettier';
 import path from 'node:path';
-import { includeIgnoreFile } from '@eslint/compat';
+import { fixupPluginRules, includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
+import neverthrow from 'eslint-plugin-neverthrow';
 
 const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 
@@ -23,6 +24,11 @@ export default defineConfig(
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
 			'no-undef': 'off'
+		}
+	},
+	{
+		plugins: {
+			neverthrow: fixupPluginRules(neverthrow)
 		}
 	},
 	{
