60cd860957e1ec80842a969dd6ac6a9e62a214af
Aske Gudmand-Høyer <78410939+Asguho@users.noreply.github.com>
2026-09-24 18:14:15 +0200

    Disable relative paths in kit config

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
 			preprocess: vitePreprocess(),
 			compilerOptions: { experimental: { async: true } },
 			experimental: { remoteFunctions: true },
+			paths: { relative: false },
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
